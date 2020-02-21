fcca5aa04f341d64d049c59c141d981826c742a5
Ybrayym <ybrayymjan90@gmail.com>
2020-02-21 11:23:08 -0500

    All of the katas are completed accordingly to the intructions

--- main.py
+++ main.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python
 """Implements math functions without using operators except for '+' and '-' """
 
-__author__ = "???"
+__author__ = "Ybrayym Abamov"
+
+
+import sys
 
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    return x + y
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    counter = 0
+    if y < 0:
+        for num in range(abs(y)):
+            counter = add(-x, counter)
+    for num in range(y):
+        counter = add(x, counter)
+    return counter
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    counter = 1
+    for _ in range(n):
+        counter = multiply(counter, x)
+    return counter
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
-
+    if x <= 1:
+        return 1
+    else:
+        for num in range(1, x):
+           x = multiply(x, num)
+    return x
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    a, b = 0, 1
+    for _ in range(1, n):
+        a, b = b, add(a, b)
+    return b
 
 
-if __name__ == '__main__':
-    # your code to call functions above
-    pass
+# def main():
+#     option = sys.argv[1]
+#     if option == 'add':
+#         print(add(2, 4))
+#     elif option == 'multiply':
+#         print(multiply(6, -8))
+#     elif option == 'power':
+#         print(power(2, 8))
+#     elif option == 'factorial':
+#         print(factorial(4))
+
+
+# if __name__ == '__main__':
+#     main()
